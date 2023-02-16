8d6154d94a6b3a5c03271f295011ee3b3c728d52
leslieportiz <lorraineportiz@gmail.com>
2023-02-15 21:57:30 -0800

    Navbar with style

--- views/default.jsx
+++ views/default.jsx
@@ -1,6 +1,6 @@
 const React = require('react')
 
-function Def (html) {
+function Def(html) {
     return (
         <html>
             <head>
@@ -9,13 +9,27 @@ function Def (html) {
                 {/* tried to copy link from cdn but className center not working
                 tried to install npm i bootstrap and used the bootstrap link directly, still nothing */}
                 {/* <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous"></link> */}
-                <link rel="stylesheet" href="/css/style.css"/>
+                <link rel="stylesheet" href="/css/style.css" />
             </head>
             <body>
+                <nav>
+                    <ul>
+                        <li>
+                            <a href="/">Home</a>
+                        </li>
+                        <li>
+                            <a href="/places">Places</a>
+                        </li>
+                        <li>
+                            <a href="/places/new">Add Place</a>
+                        </li>
+                    </ul>
+                </nav>
+
                 {html.children}
             </body>
         </html>
     )
-  }
-  
+}
+
 module.exports = Def
--- views/home.jsx
+++ views/home.jsx
@@ -5,7 +5,7 @@ function home() {
   return (
     <Def>
       <main>
-        <h1>HOME</h1>
+        <h1>REST-Rant</h1>
         <div>
           <img src="/images/fruity-smoothie.jpg" width="250" height="250
           " alt="Fruit Shake" />
