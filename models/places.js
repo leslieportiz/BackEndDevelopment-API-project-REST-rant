de33486a5353fd2bb9dba47142d376c3fd78585a
leslieportiz <lorraineportiz@gmail.com>
2023-03-02 12:39:11 -0800

    Added seeder for Comments

--- models/places.js
+++ models/places.js
@@ -10,7 +10,9 @@ const placeSchema = new mongoose.Schema({
       type: Number,
       min: [1673, 'Surely not that old?!'],
       max: [new Date().getFullYear(), 'Hey, this year is in the future!']
-    }
+    },
+    comments: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Comment' }]
+
   })
   
 
@@ -19,4 +21,3 @@ placeSchema.methods.showEstablished = function () {
 }
 
 module.exports = mongoose.model('Place', placeSchema)
-  
